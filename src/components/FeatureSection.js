175b2fdec1bb4f47ad171c8e980c334772119871
Bappy dase <bappydas.bd51@gmail.com>
2024-09-15 18:23:16 +0600

    update donload counter

--- src/components/FeatureSection.js
+++ src/components/FeatureSection.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import games1 from "../assets/games_image/code_cracker_1.jpeg"
+// import axios from 'axios';
 
 const FeatureSection = () => {
+    const [totalCounter, setTotalCounter] = useState({});
+    const getDownloader = async () => {
+        try {
+            const url = 'http://localhost:9900/total-downloader';
+            const response = await fetch(url);
+            const data = await response.json();
+            setTotalCounter(data);
+        } catch (error) {
+            console.error('Error fetching totalCounter:', error);
+        }
+    };
+    useEffect(() => {
+        getDownloader();
+    }, []);
+
+    // Function to make a POST request with an empty body
+    const updateDownloaderCount = async () => {
+        try {
+            const url = `http://localhost:9900/create-update-downloader`;
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            const data = await response.json();
+            if(data.result.acknowledged === true) {
+                getDownloader(); 
+            }
+            console.log(data);
+        } catch (error) {
+            console.error('Error during POST request:', error);
+        }
+    };
     return (
         <div className='rushpath_primary_bg'>
             <div className="mx-auto w-full max-w-screen-xl p-4 pb-20">
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-1 lg:grid-cols-1 py-8 lg:py-8">
-                    <div className='flex justify-center items-center'>
+                    <div className='flex justify-center items-center' onClick={updateDownloaderCount} >
                         <a href='https://drive.google.com/file/d/1M4wDlzsnNOEeQQbSwP34wiSC3aWEDWLG/view' target='_blank'>
                             <div className="group relative flex justify-center m-3 cursor-pointer border-[#545454] border-4">
                                 <img className="" alt='p' src={games1} />
@@ -19,10 +54,16 @@ const FeatureSection = () => {
                             </div>
                         </a>
                     </div>
+                    <div className='flex justify-center items-center'>
+                    {totalCounter ? (
+                        <p>Total Downloads: {totalCounter[0]?.totalDownload}</p>
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
-
 export default FeatureSection;
